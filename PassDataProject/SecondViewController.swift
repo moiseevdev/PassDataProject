2a36b5d7ed87947f0a233e0329e154af354c13de
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-02-11 23:10:01 +0300

    add basic functionality and design

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Андрей Моисеев on 09.02.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var login: String?
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)"
+    }
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "unwindSegue", sender: nil)
+    }
+    
+
+}
+
